153c2f133cd912bf092bdb4970d04605d4994d1a
S3OD177 <saudsa517@gmail.com>
2022-01-20 15:04:58 +0300

    Create Calculate the Transpose of a 2 by 2 matrix.cpp

---
+++ Calculate the Transpose of a 2 by 2 matrix.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+   int a[2][2],b[2][2];
+   cout<<"Enter the matrix 2x2: "<<endl;
+   for(int r=0; r<2; r++)
+   {
+   
+   		for(int c=0; c<2; c++)
+   		{
+		   
+      		cin>>a[r][c];
+	}
+}
+    for (int r = 0; r < 2; r++)
+    {
+        for (int c = 0; c < 2; c++)
+        {
+            b[c][r] = a[r][c];	
+        }
+    }
+cout << "Transpose Matrix : " << endl;
+    for (int r = 0; r < 2; r++)
+    {
+        for (int c = 0; c < 2; c++)
+        {
+            cout << b[r][c] << "\t";	
+        }
+        cout << endl;
+    }
+
+
+   return 0;
+}
